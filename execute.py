032ff39c1c4386f81ca6a7a2b868752c3bdb69e3
kamysheblid <kamysheblid@gmail.com>
2021-05-25 20:24:31 +0430

    removed useless dates

--- execute.py
+++ execute.py
@@ -27,31 +27,31 @@ def print_europe_info():
     d.plot_prediction_deaths_countries(european_countries,
                                              plot=False, extrapolate=30,
                                              prediction_days=10)
-    plt.savefig(f'{datadir}/european_{today}')
+    plt.savefig(f'{datadir}/european')
     plt.close('all')
 
 def print_important_regions():
     d.plot_prediction_deaths_countries(corona.important_regions,
                                              plot=False, extrapolate=30,
                                              prediction_days=10)
-    plt.savefig(f'{datadir}/imporant_{today}')
+    plt.savefig(f'{datadir}/imporant')
     plt.close('all')
 
 def init_conditions():
-    name = f'deaths_threshold_{today}'
+    name = f'deaths_threshold'
     d.plot_deaths_from_threshold(plot=False)
-    plt.savefig(f'{datadir}/deaths_threshold_{today}')
+    plt.savefig(f'{datadir}/deaths_threshold')
     plt.close('all')
 
 def deaths_per100k():
     d.plot_deaths_per_100k_countries(corona.important_regions, plot=False)
-    plt.savefig(f'{datadir}/deaths_per100k_{today}')
+    plt.savefig(f'{datadir}/deaths_per100k')
     plt.close('all')
 
 def canada_plot():
     (data:=canada.canada)[data>0].dropna(axis=1,how='all').dropna(axis=0,how='all').drop('Diamond Princess').T.plot(logy=True)
     plt.ylabel('Deaths')
-    plt.savefig(f'{datadir}/canada_{today}')
+    plt.savefig(f'{datadir}/canada')
     plt.close('all')
     return
 
@@ -65,7 +65,7 @@ def canada_percapita_plot():
             .T.plot(logy=True))
     #plt.grid(axis='both',which='both')
     plt.ylabel('Deaths per 100k population')
-    plt.savefig(f'{datadir}/canada_percapita_{today}')
+    plt.savefig(f'{datadir}/canada_percapita')
     plt.close('all')
     return
 
@@ -74,7 +74,7 @@ def print_canada():
     (d.raw_deaths.groupby(['Country/Region','Province/State']).sum().set_axis(d.deaths.columns,axis=1).loc['Canada'].diff(axis=1).sort_values(d.deaths.columns[-1],ascending=False).T.rolling(7).sum()/7).plot(logy=False)
     plt.title('Canada Daily Deaths')
     plt.ylabel('Deaths')
-    plt.savefig(f'{datadir}/canada_dailydeaths_{today}')
+    plt.savefig(f'{datadir}/canada_dailydeaths')
     plt.close('all')
 
 def daily_deaths(avg_days=14):
@@ -87,7 +87,7 @@ def daily_deaths(avg_days=14):
     plt.ylabel('Deaths')
     plt.xlabel('Date')
     fig = ax.get_figure()
-    fig.savefig(f'{datadir}/daily_deaths_{today}')
+    fig.savefig(f'{datadir}/daily_deaths')
     plt.close('all')
     return
 
@@ -99,7 +99,7 @@ def daily_avg_by_week_deaths():
     plt.ylabel('Deaths')
     plt.xlabel('Date')
     fig = ax.get_figure()
-    fig.savefig(f'{datadir}/weekly_deaths_{today}')
+    fig.savefig(f'{datadir}/weekly_deaths')
     plt.close('all')
     return
 
